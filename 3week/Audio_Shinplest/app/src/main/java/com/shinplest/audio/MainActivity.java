7fb69d080209f205fcac1c03cd410332b93ad8bf
shinplest <shinplest@gmail.com>
2020-03-16 22:35:46 +0900

    music finish

--- 3week/Audio_Shinplest/app/src/main/java/com/shinplest/audio/MainActivity.java
+++ 3week/Audio_Shinplest/app/src/main/java/com/shinplest/audio/MainActivity.java
@@ -4,44 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp = new MediaPlayer();
+    int length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
 //        mp = MediaPlayer.create(this, R.raw.avengers);
 //        mp.setLooping(false);
 //        mp.start();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         mp = MediaPlayer.create(this, R.raw.avengers);
         mp.setLooping(false);
+        mp.seekTo(length);
         mp.start();
     }
 
     @Override
     protected void onStop() {
+        super.onStop();
         if (mp.isPlaying()) {
-            mp.stop();
+            mp.pause();
+            length = mp.getCurrentPosition();
             mp.release();
         }
-        super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-//        if (mp.isPlaying()) {
-//            mp.stop();
-//            mp.release();
-//        }
         super.onDestroy();
+        if (mp.isPlaying()) {
+            mp.pause();
+            length = mp.getCurrentPosition();
+            mp.release();
+        }
     }
+
 }
